1e56aca92c465cedd118e5f2513dca01d01a226e
kiyoshi.yuda <suketa.kiyo@gmail.com>
2021-09-25 12:15:39 +0900

    UpdateServlet.javaを作成

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -30,11 +30,9 @@ public class UpdateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからメッセージのIDを取得して
-            // 該当のIDのメッセージ1件のみをデータベースから取得
             Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
-            // フォームの内容を各フィールドに上書き
+      
             String title = request.getParameter("title");
             m.setTitle(title);
 
@@ -42,7 +40,7 @@ public class UpdateServlet extends HttpServlet {
             m.setContent(content);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
+            m.setUpdated_at(currentTime);       
 
             // データベースを更新
             em.getTransaction().begin();
